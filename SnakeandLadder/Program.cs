6eea11a2699338ffb7525f2aa58f269cd8f43c79
Aman <amankr97111@gmail.com>
2022-08-18 16:31:37 +0530

    use case 3

--- SnakeandLadder/Program.cs
+++ SnakeandLadder/Program.cs
@@ -7,7 +7,35 @@ using System.Threading.Tasks;
 namespace SnakeAndLadder
 {
     class Program
+
+
+
     {
+
+        public static int playingOptions(int diceRoll, int dicePosition, int option  )
+        {
+            switch(option)
+            {
+                case 0:
+                    dicePosition = dicePosition - diceRoll;
+                    break;
+
+                case 1:
+                    dicePosition = dicePosition + diceRoll;
+                    break;
+
+                default:
+                    dicePosition = dicePosition;
+                    break;
+            }
+
+            return dicePosition;
+
+        }
+
+            
+
+
         static void Main(string[] args)
         {
 
@@ -18,6 +46,13 @@ namespace SnakeAndLadder
             
             Console.WriteLine(" Dice position after rolling is " +diceRoll);
             Console.ReadLine();
+
+            int option = random.Next(0,3);
+            int dicePosition = startPosition;
+
+            dicePosition = playingOptions(diceRoll,dicePosition,option);
+            Console.WriteLine("Dice position after dice roll is " + dicePosition);
+            
            
         }
     }
